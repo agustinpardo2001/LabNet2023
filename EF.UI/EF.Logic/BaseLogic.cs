8cf9bbab68260e188ca4e516159c6c1c825b7eef
agustin <agustinpardo999@gmail.com>
2023-09-01 23:57:23 -0300

    EF practica

---
+++ EF.UI/EF.Logic/BaseLogic.cs
@@ -0,0 +1,21 @@
+﻿using EF.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace EF.Logic
+{
+    public class BaseLogic
+    {
+        protected readonly NorthWindContext context;
+
+        public BaseLogic()
+        {
+            context = new NorthWindContext();
+        }
+
+    }
+}
---
+++ EF.UI/Public/CompanyNameEmptyException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C.Exceptions
+{
+    public class CompanyNameEmptyException : Exception
+    {
+        public CompanyNameEmptyException() : base("No se ingreso nada")
+        {
+
+        }
+    }
+}
